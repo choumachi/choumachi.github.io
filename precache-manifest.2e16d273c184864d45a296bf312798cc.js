7343cd648f699835e4d00cca310016355ac7c3ac
choumachi <87934576+choumachi@users.noreply.github.com>
2022-06-29 17:08:50 +0900

    Updates

---
+++ precache-manifest.2e16d273c184864d45a296bf312798cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b55867fbf9fd1b09c07",
+    "url": "/myProfilePage/static/css/main.f7a9327d.chunk.css"
+  },
+  {
+    "revision": "5b55867fbf9fd1b09c07",
+    "url": "/myProfilePage/static/js/main.5b55867f.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/myProfilePage/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "5046fe3614e9a16b227a",
+    "url": "/myProfilePage/static/js/runtime~main.5046fe36.js"
+  },
+  {
+    "revision": "5a8c2a4a840eac65e9593977bca2ca25",
+    "url": "/myProfilePage/index.html"
+  }
+];
