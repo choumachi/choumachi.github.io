22630e77665305513c3388b6ea69f359ccae9fe1
choumachi <87934576+choumachi@users.noreply.github.com>
2022-07-03 20:31:52 +0900

    ProfilePic Update

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,115 +1,117 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Choumachi",
-    "role": "프론트엔드 개발자",
-    "linkedinId":"choumachi1125@gmail.com",
-    "skypeid": "Your skypeid",
-    "roleDescription": "안녕하세요, 저는 풀스택 개발과 인공지능 및 데이터 과학을 공부하고 있는 쵸마치라고 합니다.",
-    "socialLinks":[
-        {
-          "name":"github",
-          "url":"https://github.com/choumachi",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"instagram",
-          "url":"https://www.instagram.com/chou.machi/",
-          "className":"fa fa-instagram"
-        }
-      ],
-    "aboutme":"저는 현재 대학교에서 컴퓨터공학을 전공중인 학생입니다. 현업에 종사할 수 있도록 열심히 실력을 키우고 있으며 남들과는 다른 강점을 만들기 위해 노력하고 있습니다.",
-    "address":"Busan, Korea, Republic of",
-    "website":"",
-    "education":[
-      {
-        "UniversityName":"KyungSung Univ.",
-        "specialization":"Undergraduate",
-        "MonthOfPassing":"Mar",
-        "YearOfPassing":"2020",
-        // "Achievements":"Some Achievements"
-      },
-    ],
-    "work":[
-      {
-        "CompanyName":"as Freelancer",
-        "specialization":"Artificial Intelligence Developer Certificate Lv.2",
-        "MonthOfLeaving":"May",
-        "YearOfLeaving":"2022",
-        // "Achievements":"Some Achievements"
-      },
-      // {
-      //   "CompanyName":"Some Company",
-      //   "specialization":"Some specialization",
-      //   "MonthOfLeaving":"Jan",
-      //   "YearOfLeaving":"2018",
-      //   "Achievements":"Some Achievements"
-      // }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS3"
-      },
-      {
-        "skillname":"Javascript ES6"
-      },
-      {
-        "skillname":"jQuery"
-      },
-      {
-        "skillname":"Reactjs"
-      },
-      {
-        "skillname":"NodeJS"
-      },
-      {
-        "skillname":"Photoshop"
-      },
-      {
-        "skillname":"Illustrator"
-      },
-      {
-        "skillname":"After_Effects"
-      },
-      {
-        "skillname":"Premiere Pro"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"아무 것도 하지 않으면 아무 것도 일어나지 않는다",
-        "name":"기시미 이치로"
-      },
-      // {
-      //   "description":"아무 것도",
-      //   "name":"Some technical guy"
-      // }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Choumachi",
+  role: "프론트엔드 개발자",
+  linkedinId: "choumachi1125@gmail.com",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "안녕하세요, 저는 풀스택 개발과 인공지능 및 데이터 과학을 공부하고 있는 쵸마치라고 합니다.",
+  socialLinks: [
+    {
+      name: "github",
+      url: "https://github.com/choumachi",
+      className: "fa fa-github",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/chou.machi/",
+      className: "fa fa-instagram",
+    },
+  ],
+  aboutme:
+    "저는 현재 대학교에서 컴퓨터공학을 전공중인 학생입니다. 현업에 종사할 수 있도록 열심히 실력을 키우고 있으며 남들과는 다른 강점을 만들기 위해 노력하고 있습니다.",
+  address: "Busan, Korea, Republic of",
+  website: "",
+  education: [
+    {
+      UniversityName: "KyungSung Univ.",
+      specialization: "Undergraduate",
+      MonthOfPassing: "Mar",
+      YearOfPassing: "2020",
+      // "Achievements":"Some Achievements"
+    },
+  ],
+  work: [
+    {
+      CompanyName: "as Freelancer",
+      specialization: "Artificial Intelligence Developer Certificate Lv.2",
+      MonthOfLeaving: "May",
+      YearOfLeaving: "2022",
+      // "Achievements":"Some Achievements"
+    },
+    // {
+    //   "CompanyName":"Some Company",
+    //   "specialization":"Some specialization",
+    //   "MonthOfLeaving":"Jan",
+    //   "YearOfLeaving":"2018",
+    //   "Achievements":"Some Achievements"
+    // }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS3",
+    },
+    {
+      skillname: "Javascript ES6",
+    },
+    {
+      skillname: "jQuery",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "NodeJS",
+    },
+    {
+      skillname: "Photoshop",
+    },
+    {
+      skillname: "Illustrator",
+    },
+    {
+      skillname: "After_Effects",
+    },
+    {
+      skillname: "Premiere Pro",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "아무 것도 하지 않으면 아무 것도 일어나지 않는다",
+      name: "기시미 이치로",
+    },
+    // {
+    //   "description":"아무 것도",
+    //   "name":"Some technical guy"
+    // }
+  ],
+};
+
+export default resumeData;
